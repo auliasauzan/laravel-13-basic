4508aacb03d64dbbcf793eba30552e15044e5c01
auliasauzan <auliasauzan06@gmail.com>
2026-04-08 12:41:31 +0800

    Pertemuan 2: Blade Template

---
+++ resources/views/student/crate.blade.php
@@ -0,0 +1,7 @@
+<x-app>
+
+    <x-slot:title>{{ $title }}</x-slot>
+
+    <h1 Class = "fw-bold "> Create Student </h1>
+
+</x-app>
---
+++ resources/views/student/index.blade.php
@@ -0,0 +1,7 @@
+<x-app>
+
+    <x-slot:title>{{ $title }}</x-slot>
+
+    <h1 Class = "fw-bold ">Student Data </h1>
+
+</x-app>
